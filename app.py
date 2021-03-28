0770e7cfaaa1049dc353d5dd5d439474c1e326cf
Andrés Aguilar <aeaguilarn@gmail.com>
2021-03-28 13:48:23 -0400

    Added more HTML templates.

--- app.py
+++ app.py
@@ -1,9 +1,22 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
     return render_template("index.html")
+
+@app.route('/badDogs')
+def badDogs():
+    return "There are no bad dogs in this world."
+
+@app.route('/goodDogs')
+def goodDogs():
+    return render_template("good-dogs.html")
+
+@app.route('/myDog/<name>')
+def yourDog(name):
+    return "My dog's name is " + name + "."
 
 app.run()
